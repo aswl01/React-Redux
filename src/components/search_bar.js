912eb9161d8d6f29b0ee5ccdb8f7ff0a0d270bc6
aswl01 <sweilun@umich.edu>
2018-02-08 18:08:59 -0500

    React Sstartup

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,10 @@
 /**
  * Created by sweilun on 2/8/2018.
  */
+import React from 'react';
+
+const SearchBar = () => {
+    return <input />;
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDxHG6dPRxU7M9Omr0Kf_ZG86EpByL3QcM';
+
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar/>
+    </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
