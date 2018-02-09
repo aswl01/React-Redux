1e8ec3483b1a6bf9e5912f8e9bef1e34347285ff
aswl01 <sweilun@umich.edu>
2018-02-08 21:50:33 -0500

    React Sstartup

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,23 @@
 /**
  * Created by sweilun on 2/8/2018.
  */
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />;
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={(event) => this.setState({term: event.target.value}) }/>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
